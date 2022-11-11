002c22aee2247363194e55d59435607fd0bc5f45
Rocco Razzino <roccorazzino@Roccos-MacBook-Pro.local>
2022-11-11 17:08:51 -0500

    added dashboard and code urls and views

--- users/urls.py
+++ users/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('code/', views.code, name = 'code')
 ]
--- users/views.py
+++ users/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'users/index.html')
+
+def code(request):
+    return render(request, 'users/code.html')
+
+def dashboard(request):
+    return render(request, 'users/dashboard.html')
