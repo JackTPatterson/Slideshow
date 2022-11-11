8e9bb64e1884a2b5b9c6420fd5d934dd9513c322
Jack Patterson <jackpatterson@Jacks-MacBook-Pro.local>
2022-11-11 17:01:30 -0500

    commit

--- users/urls.py
+++ users/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('users.urls'))
+    path('', views.index, name='index')
 ]
